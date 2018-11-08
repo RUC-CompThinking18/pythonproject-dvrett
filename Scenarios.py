3de272701e1c12cf72d3f331b21769fdd8325897
Dakota <dakota.everett@rutgers.edu>
2018-11-08 16:07:05 -0500

    Update with scenarios

---
+++ Scenarios.py
@@ -0,0 +1,48 @@
+def game(scenario):
+	success = 0 #measure of user's success in life
+	
+	if game("school"):
+	#scenario - You're overwhelmed with coming out and the ramifications of that on top of school.
+	#In the wake of a very difficult semester, you are on academic probation and have to decide how to move forward.
+	#option 1
+	#option 2
+	#option 3
+		if raw_input.lower() == "option 1"
+		
+		elif raw_input.lower() == "option 2"
+		
+		elif raw_input.lower() == "option 3"
+		
+		else
+		
+	elif game("job"):
+	#scenario - You're applying for a job
+	#option 1
+	#option 2
+	#option 3
+		if raw_input.lower() == "option 1"
+		
+		elif raw_input.lower() == "option 2"
+		
+		elif raw_input.lower() == "option 3"
+		
+		else
+		
+	elif game("relationship")
+	#scenario - 
+	#option 1
+	#option 2
+	#option 3
+		if raw_input.lower() == "option 1"
+		
+		elif raw_input.lower() == "option 2"
+		
+		elif raw_input.lower() == "option 3"
+		
+		else
+		
+	
+
+	def step(self, sequence):
+		for i in sequence:
+			
--- User_Class.py
+++
@@ -1,9 +0,0 @@
-
-class User(object):
-	var work ##job
-	var education ##education status, ex: dropped out, high school, GED, bachelor's, master's, doctorate
-	var resources ##socioeconomic status, money, housing, etc.
-
-	def step(self, sequence):
-		for i in sequence:
-			
