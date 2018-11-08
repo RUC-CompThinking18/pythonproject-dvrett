a64563170818cb9a71dfcf1d1d9801461128c2e4
Dakota <dakota.everett@rutgers.edu>
2018-11-08 16:40:36 -0500

    Update3

--- Scenarios.py
+++ Scenarios.py
@@ -12,16 +12,16 @@ def game(scenario):
 		elif raw_input.lower() == "option 2":
 		
 		elif raw_input.lower() == "option 3":
-		
+			success += 5
 		else:
 			raise typeError "Input is not an option."
 		
 	elif game("job"):
-	#scenario - You're applying for a job and must choose a company and industry that will have an accepting environment
+	#scenario - You're applying for a job and must choose an industry and company that will have an accepting environment
 	#but also be sustainable for you to live on.
-	#option 1
-	#option 2
-	#option 3
+	#option 1 - Something creative, probably freelance or short-term contract-based
+	#option 2 - Something stable, probably corporate but pays decently
+	#option 3 - Whatever you can find - retail, food service, etc.
 		if raw_input.lower() == "option 1":
 		
 		elif raw_input.lower() == "option 2":
@@ -32,7 +32,7 @@ def game(scenario):
 			raise typeError "Input is not an option."
 			
 	elif game("relationship")
-	#scenario - 
+	#scenario - You're looking to date but aren't sure where to find someone
 	#option 1
 	#option 2
 	#option 3
@@ -44,8 +44,10 @@ def game(scenario):
 		
 		else:
 			raise typeError "Input is not an option."
-	
 
-	def step(self, sequence):
-		for i in sequence:
-			
+	if success >= 10:
+		print "Congratulations! You scraped your way through adversity and into a comfortable life"
+	if success >= 5 and < 10:
+		print "You've struggled through a lot and are still struggling. You don't know when or if things will get better but while you're waiting for your miracle, you've just gotta keep going"
+	if success < 5:
+		print "You've struggled and fallen more times than you can count. You feel like the world was built against you - it's left you marginalized, defeated, and homeless"
