01895adfbd9171f298de394f2419176fc7b18a8b
Dakota <dakota.everett@rutgers.edu>
2018-11-08 16:19:29 -0500

    Update2

--- Scenarios.py
+++ Scenarios.py
@@ -4,43 +4,46 @@ def game(scenario):
 	if game("school"):
 	#scenario - You're overwhelmed with coming out and the ramifications of that on top of school.
 	#In the wake of a very difficult semester, you are on academic probation and have to decide how to move forward.
-	#option 1
-	#option 2
-	#option 3
-		if raw_input.lower() == "option 1"
+	#option 1 - Drop out and find a job to live on
+	#option 2 - Try to finish on time at the recommended pace of 12-15 credits per semester
+	#option 3 - Switch to a part-time course load to make things more manageable
+		if raw_input.lower() == "option 1":
 		
-		elif raw_input.lower() == "option 2"
+		elif raw_input.lower() == "option 2":
 		
-		elif raw_input.lower() == "option 3"
+		elif raw_input.lower() == "option 3":
 		
-		else
+		else:
+			raise typeError "Input is not an option."
 		
 	elif game("job"):
-	#scenario - You're applying for a job
+	#scenario - You're applying for a job and must choose a company and industry that will have an accepting environment
+	#but also be sustainable for you to live on.
 	#option 1
 	#option 2
 	#option 3
-		if raw_input.lower() == "option 1"
+		if raw_input.lower() == "option 1":
 		
-		elif raw_input.lower() == "option 2"
+		elif raw_input.lower() == "option 2":
 		
-		elif raw_input.lower() == "option 3"
+		elif raw_input.lower() == "option 3":
 		
-		else
-		
+		else:
+			raise typeError "Input is not an option."
+			
 	elif game("relationship")
 	#scenario - 
 	#option 1
 	#option 2
 	#option 3
-		if raw_input.lower() == "option 1"
+		if raw_input.lower() == "option 1":
 		
-		elif raw_input.lower() == "option 2"
+		elif raw_input.lower() == "option 2":
 		
-		elif raw_input.lower() == "option 3"
+		elif raw_input.lower() == "option 3":
 		
-		else
-		
+		else:
+			raise typeError "Input is not an option."
 	
 
 	def step(self, sequence):
