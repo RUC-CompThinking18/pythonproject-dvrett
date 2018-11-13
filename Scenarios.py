435a5254e8126bbc267151ec91763cd2f83d53f4
Dakota <dakota.everett@rutgers.edu>
2018-11-13 13:31:17 -0500

    3 function update

--- Scenarios.py
+++ Scenarios.py
@@ -1,21 +1,22 @@
-def game(scenario):
-	success = 0 #measure of user's success in life
-	if game("school"):
-		print("""Scenario: You're overwhelmed with coming out and the ramifications of that on top of school./nIn the wake of a very difficult semester, you are on academic probation and have to decide how to move forward.
-		/nOption 1 - Drop out and find a job to live on
+success = 0 #measure of user's success in life
+
+def school():
+	print """Scenario: You're overwhelmed with coming out and the ramifications of that on top of school./nIn the wake of a very difficult semester, you are on academic probation and have to decide how to move forward.
+	/nOption 1 - Drop out and find a job to live on
 		/nOption 2 - Try to finish on time at the recommended pace of 12-15 credits per semester
-		/nOption 3 - Switch to a part-time course load to make things more manageable, but take much longer to get a living wage""")
+		/nOption 3 - Switch to a part-time course load to make things more manageable, but take much longer to get a living wage"""
 		choice = raw_input().lower()
 		if choice == "option 1":
-			success +=
+			success += 2
 		elif choice == "option 2":
-			success +=
+			success += 3
 		elif choice == "option 3":
-			success += 5
+			success += 4
 		else:
-			raise typeError "Input is not a valid option."
+			print "Input is not a valid option."
+			school()
 
-	elif game("job"):
+	def job():
 	#scenario - You're applying for a job and must choose an industry and company that will have an accepting environment
 	#but also be sustainable for you to live on.
 	#option 1 - Something creative, probably freelance or short-term contract-based
@@ -29,9 +30,9 @@ def game(scenario):
 		elif choice == "option 3":
 			success += 1
 		else:
-			raise typeError "Input is not a valid option."
+			print "Input is not a valid option."
 
-	elif game("housing"):
+	def housing():
 	#scenario - You're looking to rent an apartment or house with your partner. You need to choose a neighborhood and type of landlord.
 	#option 1 -
 	#option 2 -
@@ -44,7 +45,7 @@ def game(scenario):
 		elif choice == "option 3":
 			success +=
 		else:
-			raise typeError "Input is not a valid option."
+			print "Input is not a valid option."
 
 	if success >= 10:
 		print("Congratulations! You scraped your way through adversity and into a comfortable life")
