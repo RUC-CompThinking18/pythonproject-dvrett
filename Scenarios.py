13d219306869360b4585e0d0c70884695047c4bc
Dakota <dakota.everett@rutgers.edu>
2018-11-20 16:50:00 -0500

    Update 7

--- Scenarios.py
+++ Scenarios.py
@@ -7,9 +7,26 @@ class Question():
 	
 	def __init__(self):
 		# Each question has a scenario text and options text
-		self.scenario
-		self.options
+		
+		# School scenario
+		self.school = """Scenario: You're overwhelmed with coming out and the ramifications of that on top of school./nIn the wake of a very difficult semester, you are on academic probation and have to decide how to move forward.
+		/nOption 1 - Drop out and find a job to live on
+		/nOption 2 - Try to finish on time at the recommended pace of 12-15 credits per semester
+		/nOption 3 - Switch to a part-time course load to make things more manageable, but take much longer to get a living wage"""
+		
+		# Job scenario
+		self.job = """Scenario: You're applying for a job and must choose an industry and company that will have an accepting environment but also be sustainable for you to live on.
+		/nOption 1 - Something creative, probably freelance or short-term contract-based
+		/nOption 2 - Something stable, probably corporate but pays decently
+		/nOption 3 - Whatever you can find - retail, food service, etc."""
+		
+		# Housing scenario
+		self.housing = """Scenario: You're looking to rent an apartment or house with your partner. You need to choose a neighborhood and type of landlord.
+		/nOption 1 -
+		/nOption 2 -
+		/nOption 3 -"""
 
+	# Function to present question and reassess success variable
 	def ask_question(question):
 		print question.text
 		choice = raw_input().lower()
@@ -22,29 +39,6 @@ class Question():
 		else:
 			print "Input is not a valid option."
 			ask_question(question)
-
-
-		# School scenario
-		"""Scenario: You're overwhelmed with coming out and the ramifications of that on top of school./nIn the wake of a very difficult semester, you are on academic probation and have to decide how to move forward.
-		/nOption 1 - Drop out and find a job to live on
-		/nOption 2 - Try to finish on time at the recommended pace of 12-15 credits per semester
-		/nOption 3 - Switch to a part-time course load to make things more manageable, but take much longer to get a living wage"""
-		
-
-		# Job scenario
-		"""Scenario: You're applying for a job and must choose an industry and company that will have an accepting environment but also be sustainable for you to live on.
-		/nOption 1 - Something creative, probably freelance or short-term contract-based
-		/nOption 2 - Something stable, probably corporate but pays decently
-		/nOption 3 - Whatever you can find - retail, food service, etc."""
-	
-		# Housing scenario
-		print """Scenario: You're looking to rent an apartment or house with your partner. You need to choose a neighborhood and type of landlord.
-		/nOption 1 -
-		/nOption 2 -
-		/nOption 3 -"""
-	
-	print ""
-
 
 	if success >= 10:
 		print("Congratulations! You scraped your way through adversity and into a comfortable life")
