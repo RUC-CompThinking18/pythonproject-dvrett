c72ecb15bd3677270f33528d691035a60517069c
Dakota <dakota.everett@rutgers.edu>
2018-11-20 16:31:32 -0500

    Encapsulation start

--- Scenarios.py
+++ Scenarios.py
@@ -1,10 +1,17 @@
-success = 0 #measure of user's success in life
+# -*- coding: utf-8 -*-
 
-def school():
-	print """Scenario: You're overwhelmed with coming out and the ramifications of that on top of school./nIn the wake of a very difficult semester, you are on academic probation and have to decide how to move forward.
-	/nOption 1 - Drop out and find a job to live on
-		/nOption 2 - Try to finish on time at the recommended pace of 12-15 credits per semester
-		/nOption 3 - Switch to a part-time course load to make things more manageable, but take much longer to get a living wage"""
+success = 0 # Measure of user's success in life
+
+class Question():
+	# Question simulates a scenario with options being presented to the user
+	
+	def __init__(self):
+		# Each question has a scenario text and options text
+		self.scenario
+		self.options
+
+	def ask_question(question):
+		print question.text
 		choice = raw_input().lower()
 		if choice == "option 1":
 			success += 2
@@ -14,38 +21,30 @@ def school():
 			success += 4
 		else:
 			print "Input is not a valid option."
-			school()
+			ask_question(question)
 
-	def job():
-	#scenario - You're applying for a job and must choose an industry and company that will have an accepting environment
-	#but also be sustainable for you to live on.
-	#option 1 - Something creative, probably freelance or short-term contract-based
-	#option 2 - Something stable, probably corporate but pays decently
-	#option 3 - Whatever you can find - retail, food service, etc.
-		choice = raw_input().lower()
-		if choice == "option 1":
-			success += 2
-		elif choice == "option 2":
-			success += 3
-		elif choice == "option 3":
-			success += 1
-		else:
-			print "Input is not a valid option."
 
-	def housing():
-	#scenario - You're looking to rent an apartment or house with your partner. You need to choose a neighborhood and type of landlord.
-	#option 1 -
-	#option 2 -
-	#option 3 -
-		choice = raw_input().lower()
-		if choice == "option 1":
-			success +=
-		elif choice == "option 2":
-			success +=
-		elif choice == "option 3":
-			success +=
-		else:
-			print "Input is not a valid option."
+		# School scenario
+		"""Scenario: You're overwhelmed with coming out and the ramifications of that on top of school./nIn the wake of a very difficult semester, you are on academic probation and have to decide how to move forward.
+		/nOption 1 - Drop out and find a job to live on
+		/nOption 2 - Try to finish on time at the recommended pace of 12-15 credits per semester
+		/nOption 3 - Switch to a part-time course load to make things more manageable, but take much longer to get a living wage"""
+		
+
+		# Job scenario
+		"""Scenario: You're applying for a job and must choose an industry and company that will have an accepting environment but also be sustainable for you to live on.
+		/nOption 1 - Something creative, probably freelance or short-term contract-based
+		/nOption 2 - Something stable, probably corporate but pays decently
+		/nOption 3 - Whatever you can find - retail, food service, etc."""
+	
+		# Housing scenario
+		print """Scenario: You're looking to rent an apartment or house with your partner. You need to choose a neighborhood and type of landlord.
+		/nOption 1 -
+		/nOption 2 -
+		/nOption 3 -"""
+	
+	print ""
+
 
 	if success >= 10:
 		print("Congratulations! You scraped your way through adversity and into a comfortable life")
