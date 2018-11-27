6f56346efa56058a1f389a79084ad5c607eb92a0
Dakota <dakota.everett@rutgers.edu>
2018-11-27 12:27:37 -0500

    Update 8

--- Scenarios.py
+++ Scenarios.py
@@ -4,27 +4,27 @@ success = 0 # Measure of user's success in life
 
 class Question():
 	# Question simulates a scenario with options being presented to the user
-	
+
 	def __init__(self):
 		# Each question has a scenario text and options text
-		
+
 		# School scenario
 		self.school = """Scenario: You're overwhelmed with coming out and the ramifications of that on top of school./nIn the wake of a very difficult semester, you are on academic probation and have to decide how to move forward.
 		/nOption 1 - Drop out and find a job to live on
 		/nOption 2 - Try to finish on time at the recommended pace of 12-15 credits per semester
 		/nOption 3 - Switch to a part-time course load to make things more manageable, but take much longer to get a living wage"""
-		
+
 		# Job scenario
 		self.job = """Scenario: You're applying for a job and must choose an industry and company that will have an accepting environment but also be sustainable for you to live on.
 		/nOption 1 - Something creative, probably freelance or short-term contract-based
 		/nOption 2 - Something stable, probably corporate but pays decently
 		/nOption 3 - Whatever you can find - retail, food service, etc."""
-		
+
 		# Housing scenario
 		self.housing = """Scenario: You're looking to rent an apartment or house with your partner. You need to choose a neighborhood and type of landlord.
-		/nOption 1 -
-		/nOption 2 -
-		/nOption 3 -"""
+		/nOption 1 - Private landlord with one-bedroom apartment.
+		/nOption 2 - Move in with friends or strangers who have an opening in an affordable apartment.
+		/nOption 3 - Property management company with more expensive one-bedroom apartment. """
 
 	# Function to present question and reassess success variable
 	def ask_question(question):
@@ -46,3 +46,7 @@ class Question():
 		print("You've struggled through a lot and are still struggling. You don't know when or if things will get better but while you're waiting for your miracle, you've just gotta keep going")
 	if success < 5:
 		print("You've struggled and fallen more times than you can count. You feel like the world was built against you - it's left you marginalized, defeated, and homeless")
+
+Question(self.school)
+Question(self.job)
+Question(self.housing)
